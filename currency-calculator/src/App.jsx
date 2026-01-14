9ac0d6a5c5a5ba8c7f71c92de809c473c29815f4
jooho2075 <parkjh2075@icloud.com>
2026-01-14 10:02:03 +0900

    개인 브랜치 생성 테스트

--- currency-calculator/src/App.jsx
+++ currency-calculator/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import DefaultLayout from './layouts/DefaultLayout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div></div>  
+    <div>
+      <DefaultLayout>
+        <h1>안녕하세요</h1>
+      </DefaultLayout>
+    </div>  
   )
 }
 
---
+++ currency-calculator/src/components/ui/Modal.jsx
@@ -0,0 +1,9 @@
+const Modal = () => {
+    return(
+        <>
+        Modal창 입니다.
+        </>
+    )
+}
+
+export default Modal;

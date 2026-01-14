11f95eae30b23b36dc8cedbdd13a9fe2aa22c54a
urimL <toulim0525@gmail.com>
2026-01-14 14:18:52 +0900

    Modal event 수정

--- currency-calculator/src/components/BankButton.jsx
+++ currency-calculator/src/components/BankButton.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CurrencyForm from './CurrencyForm';
+import Modal from './ui/Modal';
+import { createPortal } from 'react-dom';
 
-const BankButton = ({bank}) => {
+
+const BankButton = ({ bank }) => {
+  const [openModal, open] = useState(false);
+  const closeModal = () => open(false);
+  
   return (
-    <button
+    <>
+      <button
         key={bank.id}
-        onClick={() => {
-            console.log("버튼 눌림");
-        }
+        onClick={() => open(true)
         }
         className="m-2 px-6 py-2 font-semibold text-gray-100 bg-gray-800 border-none rounded cursor-pointer"
         data-cy="add-todo-button">
         {bank.title}
-    </button>
+      </button>
+
+      {openModal && createPortal(
+        <Modal onClose={closeModal}>
+          <CurrencyForm
+            bankInfo={bank}
+            onClose={() => open(false)}
+          />
+        </Modal>,
+        document.body
+      )}
+    </>
+
   )
 }
 

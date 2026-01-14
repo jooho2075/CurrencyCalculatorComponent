52c8e02f8919fb04c424c72cffdd36ff15496145
jooho2075 <parkjh2075@icloud.com>
2026-01-14 12:29:57 +0900

    환율금액 입력 결과 표시

--- currency-calculator/src/components/ui/Modal.jsx
+++ currency-calculator/src/components/ui/Modal.jsx
@@ -3,7 +3,7 @@ import CurrencyForm from "../CurrencyForm";
 const Modal = ({ children }) => {
     return(
         <div className="w-96 bg-sky-400 rounded-lg">
-            <h1>모달창입니다.</h1>
+            {/* <h1>모달창입니다.</h1> */}
             {children}
         </div>
     )
